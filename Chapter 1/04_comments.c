f7cf619d5ad8bfd964406d60e4601f69afc5b6ce
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-01 22:15:36 +0600

    1st week at c programming

---
+++ Chapter 1/04_comments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    // way
+    
+
+    printf("Hello world");
+    return 0;
+
+}
---
+++ Chapter 1/07_escape_sequence.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+int a;
+char ch = 'a';
+ch = 'b';
+ch = '\n'; //represents a new line character
+printf("this is your rocco \n baby");
+printf("this is your rocco \t baby"); //\t represents a tab space
+printf("this is your rocco \\ baby"); //\\ represents a backslash character
+printf("this is your rocco \" baby"); //\" represents a double quote character
+printf("this is your rocco \' baby"); //\' represents a single quote character
+printf("this is your rocco \? baby"); //\? represents a question mark character
+printf("this is your rocco \a baby"); //\a represents an alert or bell character
+printf("this is your rocco \b baby"); //\b represents a backspace character
+
+return 0;
+
+
+
+
+}
