52fc42426b1ef85e812253a2d6b64d8d7a5d579a
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-05 20:05:18 +0600

    My some practise from week 2

---
+++ Chapter 2 practice/01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    // float a = 3.0;
+    // int b = 8;
+    // printf("The value is %f", (a/b)-2); wrong code
+
+   float a= (3.0/8)-2;
+   printf("The value of a is %0.2f", a);
+    return 0;
+}
