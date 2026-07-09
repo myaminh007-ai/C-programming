e7d3fee7e1be0f20fca2e046759c10700eb67c43
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-09 14:22:55 +0600

    3rd week of C programming

---
+++ Chapter 3/01_conditional.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int age = 20;
+    if(age>15){
+        printf("You are older my boi\n");
+    }
+     if(age%3==0){
+        printf("You are divisible by 5");
+    }
+    return 0;
+}
