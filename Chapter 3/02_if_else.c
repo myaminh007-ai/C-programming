e86ed328a937a33e98ad9ca262b881d13e1d8af0
MD. Yamin Hossain <myaminh007@gmail.com>
2026-07-11 23:13:12 +0600

    New file

---
+++ Chapter 3/02_if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int age = 20;
+    if(age>30){
+        printf("You are older my boi\n");
+    }
+    else{
+        printf("mara khao\n");
+    }
+     
+    return 0;
+}
